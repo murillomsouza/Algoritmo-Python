aaa28f71400554bfd20eda82edbe798e9a11de6c
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:58:07 -0300

    Delete exerc010.py
    Adicionei uma pasta com todos os documentos

--- exerc010.py
+++
@@ -1,9 +0,0 @@
-"""Exercicio 10"""
-number1 = float(input('Digite o primeiro número: '))
-number2 = float(input('Digite o segundo número: '))
-if number1 % number2 == 0:
-    print('São multiplos')
-elif number2 % number1 == 0:
-    print('São multiplos')
-else:
-    print('Não são multiplos')
