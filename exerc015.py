3457f00da08c3f76c42f3af204ea57ecac233f1b
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:58:53 -0300

    Delete exerc015.py
    Adicionei uma pasta com todos os documentos

--- exerc015.py
+++
@@ -1,5 +0,0 @@
-"""Exercicio 15"""
-reais = float(input('Digite o valor a ser convertido (em reais): '))
-#considerando dolar a R$5,00
-conversao = reais/5.00
-print(f'Você teria U${conversao:.2f}, após a conversão')
