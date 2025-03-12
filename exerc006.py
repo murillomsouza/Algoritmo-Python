641407c32de7b389e8fcec3285a784571bafef98
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:57:22 -0300

    Delete exerc006.py
    Adicionei uma pasta com todos os documentos

--- exerc006.py
+++
@@ -1,5 +0,0 @@
-"""Exercicio 6"""
-base = float(input('Digite a base do triangulo:'))
-altura = float(input('Digite e altura do triangulo:'))
-areatri = (base*altura)/2
-print(f'A area do triangulo é {areatri}')
