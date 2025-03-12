60809a8eb78636db86090ae23172cc1ed740686c
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:56:45 -0300

    Delete exerc003.py
    Adicionei uma pasta com todos os documentos

--- exerc003.py
+++
@@ -1,5 +0,0 @@
-"""Excercicio 3"""
-number = float(input('Digite o número'))
-double_number = number*2
-trdnumber = number/3
-print(f'O dobre desse número é {double_number}, e sua terça parte é {trdnumber}')
