cf14147832291e6bf9d2da05b766c09f2462e418
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:58:36 -0300

    Delete exerc013.py
    Adicionei uma pasta com todos os documentos

--- exerc013.py
+++
@@ -1,5 +0,0 @@
-"""Exercicio 13"""
-distancia = float(input('Digite a distancia a ser percorrida (em Km): '))
-vlcmedia = float(input('Digite a velocidade média (em Km/h): '))
-tempo = distancia/vlcmedia
-print(f'A viagem irá demorar {tempo} horas')
