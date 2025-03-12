dd95b0ce085c70d5398901cd7a9b4fdc95b99012
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:57:52 -0300

    Delete exerc008.py
    Adicionei uma pasta com todos os documentos

--- exerc008.py
+++
@@ -1,6 +0,0 @@
-"""Exercicio 8"""
-altura = float(input('Digite a altura da parade: '))
-largura = float(input('Digite a largura da parede: '))
-area = altura*largura
-tint = area/2
-print(f'Serão necessários {tint} litros de tinta para esta parede')
