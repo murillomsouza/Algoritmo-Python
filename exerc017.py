d73296e45a039d913eefd01be8221e04f40e95d6
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:59:13 -0300

    Delete exerc017.py
    Adicionei uma pasta com todos os documentos

--- exerc017.py
+++
@@ -1,5 +0,0 @@
-"""Exercicio 17"""
-dias = int(input('Digite a quantidade de dias que o carro foi alugado: '))
-kmr = float(input('Digite quantos Km esse carro andou durante esse período: '))
-conta = (dias * 90) + (kmr * 0.20)
-print(f'O preço total desse aluguel ficou em R${conta}')
