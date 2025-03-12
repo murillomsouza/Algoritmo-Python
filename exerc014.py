f79a9c32b2bb95fc50880ffde77af4a6f2fe8fbd
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:58:45 -0300

    Delete exerc014.py
    Adicionei uma pasta com todos os documentos

--- exerc014.py
+++
@@ -1,5 +0,0 @@
-"""Exercicio 14"""
-consumo = float(input('Digite a quantidades de kV/h foram consumidos:'))
-valor = float(input('Digite o valor do kW/h (em reais): '))
-conta = consumo*valor
-print(f'Sua conta será no valor de R${conta}.')
