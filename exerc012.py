3292a6705cab9518289742dbca37cdfc54f86dbd
Murillo Miguel Ozorio de Souza <murillosouza997@gmail.com>
2025-03-12 09:58:27 -0300

    Delete exerc012.py
    Adicionei uma pasta com todos os documentos

--- exerc012.py
+++
@@ -1,4 +0,0 @@
-"""Exercicio 12"""
-salario = float(input('Digite o salario em Reais: '))
-nvsalario = salario + (salario * 15 / 100)
-print(f'O salario desse colaborador com um aumento de 15% é R${nvsalario}')
